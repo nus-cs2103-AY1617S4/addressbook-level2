3c8712a254e552ffe0f5461d6a0411e38355ed4b
radneran <a0124399@u.nus.edu>
2017-06-24 08:45:20 +0800

    Complete T3A3

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -3,57 +3,74 @@ package seedu.addressbook.data.person;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Represents a Person's address in the address book. Guarantees: immutable; is
+ * valid as declared in {@link #isValidAddress(String)}
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+	public static final String EXAMPLE = "123, some street";
+	public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+	public static final String ADDRESS_VALIDATION_REGEX = ".+";
+	private static final String PS = ", ";
 
-    public final String value;
-    private boolean isPrivate;
+	public Block block;
+	public Street street;
+	public Unit unit;
+	public PostalCode postalCode;
 
-    /**
-     * Validates given address.
-     *
-     * @throws IllegalValueException if given address string is invalid.
-     */
-    public Address(String address, boolean isPrivate) throws IllegalValueException {
-        String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
-            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        }
-        this.value = trimmedAddress;
-    }
+	private boolean isPrivate;
 
-    /**
-     * Returns true if a given string is a valid person address.
-     */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
-    }
+	/**
+	 * Validates given address.
+	 *
+	 * @throws IllegalValueException
+	 *             if given address string is invalid.
+	 */
+	public Address(String address, boolean isPrivate) throws IllegalValueException {
+		String trimmedAddress = address.trim();
+		this.isPrivate = isPrivate;
+		if (!isValidAddress(trimmedAddress)) {
+			throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+		}
+		initialiseValues(trimmedAddress);
+	}
 
-    @Override
-    public String toString() {
-        return value;
-    }
+	private void initialiseValues(String address) {
+		String[] addressParts = address.split(",");
+		block = new Block(addressParts[0].trim());
+		street = new Street(addressParts[1].trim());
+		unit = new Unit(addressParts[2].trim());
+		postalCode = new PostalCode(addressParts[3].trim());
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
-    }
+	/**
+	 * Returns true if a given string is a valid person address.
+	 */
+	public static boolean isValidAddress(String test) {
+		return test.matches(ADDRESS_VALIDATION_REGEX);
+	}
 
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+	@Override
+	public String toString() {
+		return block.toString() + PS + street.toString() + PS + unit.toString() + PS + postalCode.toString();
+	}
 
-    public boolean isPrivate() {
-        return isPrivate;
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof Address // instanceof handles nulls
+						&& this.block.equals(((Address) other).block)&& this.street.equals(((Address) other).street)
+						&& this.unit.equals(((Address) other).unit)
+						&& this.postalCode.equals(((Address) other).postalCode)); // state
+																		// check
+	}
+
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+
+	public boolean isPrivate() {
+		return isPrivate;
+	}
 }
