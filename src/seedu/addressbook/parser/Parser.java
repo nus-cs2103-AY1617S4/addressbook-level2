db6503d9892c3045ba4b47ea603c3fb9916716d5
luishengjie <luishengjie8@gmail.com>
2017-06-27 02:34:45 +0800

    List all persons after an add command

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -57,6 +57,16 @@ public class Parser {
     public static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
 
     public Parser() {}
+    
+    public String getCommandWordFromCommand(String userInput){
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            return MESSAGE_INVALID_COMMAND_FORMAT;
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        return commandWord;        
+    }
 
     /**
      * Parses user input into command for execution.
