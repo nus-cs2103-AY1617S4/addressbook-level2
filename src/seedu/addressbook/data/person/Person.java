a28bd0816d0e4799bb28fbc791fdecd4d04cae3c
Lim Dao Han <e0014798@u.nus.edu>
2017-06-28 15:04:54 +0800

    Added Sort Command

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -8,12 +8,17 @@ import java.util.Objects;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Person implements ReadOnlyPerson, Comparable<Person>{
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    
+    public int compareTo(Person other){
+    	return this.getName().toString().compareTo(other.getName().toString());
+    }
+ 
 
     private final UniqueTagList tags;
     /**
