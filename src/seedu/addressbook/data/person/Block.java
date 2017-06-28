dcb6e1bbdd9a2511e362b980e8f3811c9bf2feac
Richard Goh <richard_gohcr@hotmail.com>
2017-06-28 14:02:31 +0800

    no message

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,5 +1,13 @@
 package seedu.addressbook.data.person;
 
 public class Block {
-
+    private String number;
+    
+    public Block(String number){
+        this.number = number;
+    }
+    
+    public String getNumber(){
+        return number;
+    }
 }
--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -1,5 +1,14 @@
 package seedu.addressbook.data.person;
 
 public class Unit {
+    private String number;
+    
+    public Unit(String number){
+        this.number = number;
+    }
+    
+    public String getNumber(){
+        return number;
+    }
 
 }
