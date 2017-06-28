45aa4b08f1d74fa52370b40a4242b069e3f7df44
Richard Goh <richard_gohcr@hotmail.com>
2017-06-28 13:50:53 +0800

    Split address into classes

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -2,6 +2,10 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+import java.awt.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
@@ -10,9 +14,14 @@ public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_VALIDATION_REGEX = "(\\w+)(, ([^,]+)?(, ([^,]+)?(, (\\d+))?)?)?";
 
-    public final String value;
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
+    
+    public final String value; 
     private boolean isPrivate;
 
     /**
@@ -22,11 +31,20 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
+        Pattern pattern = Pattern.compile(ADDRESS_VALIDATION_REGEX);
+        Matcher matcher = pattern.matcher(trimmedAddress);
+
+        if(matcher.find()){
+            this.block = new Block(matcher.group(1));
+            this.street = new Street(matcher.group(3));
+            this.unit = new Unit(matcher.group(5));
+            this.postalCode = new PostalCode(matcher.group(7));
+            this.isPrivate = isPrivate;
+        }
+        else {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        this.value = this.toString();
     }
 
     /**
@@ -38,7 +56,12 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        String result;
+        result = block.getNumber();
+        result += " " + street.getNumber();
+        result += " " + unit.getNumber();
+        result += " " + postalCode.getNumber();
+        return result;
     }
 
     @Override
