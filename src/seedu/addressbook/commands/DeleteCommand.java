08bbd1ad9c06384f0bb2374795f6a5445a9b0783
akd13 <akankshitadash@yahoo.com>
2017-06-29 14:33:09 +0800

    Add sequenceNumber and static nextSequenceNumber to get sequence of person after adding.

--- src/seedu/addressbook/commands/DeleteCommand.java
+++ src/seedu/addressbook/commands/DeleteCommand.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
 
@@ -30,6 +31,7 @@ public class DeleteCommand extends Command {
         try {
             final ReadOnlyPerson target = getTargetPerson();
             addressBook.removePerson(target);
+            Person.decreaseNextSequenceNumber();
             return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
 
         } catch (IndexOutOfBoundsException ie) {
