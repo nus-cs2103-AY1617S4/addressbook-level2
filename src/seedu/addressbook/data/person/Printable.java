b9cc4beb41995041e3c213e7180f39b3a8fa496b
kevinLamKB <e0004543@u.nus.edu>
2017-07-02 00:07:08 +0800

    Add Printable interface and update Name,Email,Phone,Address classes that implement it

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * A read-only interface used to get a printable String from Address,Email,Name,Phone data of a Person.
+ */
+public interface Printable {
+
+        public String getPrintableString(); 
+}
