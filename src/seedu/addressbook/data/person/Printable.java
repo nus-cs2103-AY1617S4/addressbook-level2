a029417da6eea5a45fd8c3182f741b7b08041a26
Yogamurti Sutanto <yogamurtisutanto@gmail.com>
2017-07-02 16:25:19 +0800

    Added a default getPrintableString(Printable... printables) method to the Printable interface.

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,8 +1,16 @@
 package seedu.addressbook.data.person;
 
-/**
- * A read-only immutable interface to get a printable string representing a bunch of person details.
- */
+// A read-only immutable interface to get a printable string representing a bunch of person details.
+
 public interface Printable {
 	public String getPrintableString(); 
+	
+    // Returns a concatenated version of the printable strings of each object.
+    default public String getPrintableString(Printable... printables){
+    	final StringBuilder printableString = new StringBuilder();
+    	for(Printable printable:printables){
+    		printableString.append(printable.getPrintableString());
+    	}
+    	return printableString.toString();
+    }
 }
