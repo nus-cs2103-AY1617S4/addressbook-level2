315824f5cbba418b4010edd8af97083ff2feb1a7
Yogamurti Sutanto <yogamurtisutanto@gmail.com>
2017-07-02 15:51:43 +0800

    Add a Printable interface and override the getPrintableString() method for the Name, Phone, Email and Address classes.

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+/**
+ * A read-only immutable interface to get a printable string representing a bunch of person details.
+ */
+public interface Printable {
+	public String getPrintableString(); 
+}
