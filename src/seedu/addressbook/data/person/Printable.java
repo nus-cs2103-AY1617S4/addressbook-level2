3c306424e840f09fe1e2c2670f47badeb84ada88
Lim Dao Han <e0014798@u.nus.edu>
2017-07-03 14:15:43 +0800

    Creates Printable interface and implements getPrintableString functions in classes

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,5 @@
+package seedu.addressbook.data.person;
+//an interface that allows user to access data on a read-only, immutable basis
+public interface Printable {
+	public String getPrintableString();
+}
