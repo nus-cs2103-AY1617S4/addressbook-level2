4221fe9a9d22a1df9ff201c62239d7725cd089ca
akd13 <akankshitadash@yahoo.com>
2017-07-07 12:44:27 +0800

    Update Printable interface and move implemented method to Person.

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -8,15 +8,4 @@ public interface Printable {
 
     public String getPrintableString();
 
-    /**
-     * Returns a concatenated version of the printable strings of each object.
-     */
-    public static String getPrintableString(Printable... printables) {
-        StringBuilder personString = new StringBuilder(1000);
-        for (Printable printable : printables) {
-            personString.append(printable.getPrintableString()+" ");
-        }
-        return personString.toString();
-    }
-
 }
